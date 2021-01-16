51b3f2031156aea5aa1dd073d6af283d8236a64d
Kurt Guardia <kurtguardia@gmail.com>
2021-01-16 18:22:58 +0100

    Added framer motion on User

--- src/views/User/User.js
+++ src/views/User/User.js
@@ -6,6 +6,7 @@ import { Btn } from '../../components/UI';
 import { logout } from '../../store/actions/authActions';
 import { useHistory } from 'react-router-dom';
 import { english, spanish } from '../../languages';
+import { motion } from 'framer-motion';
 
 const User = () => {
   const auth = useSelector((state) => state.firebase.auth);
@@ -27,8 +28,54 @@ const User = () => {
     }
   }, [language]);
 
+  const cointainerVariants = {
+    hidden: {
+      opacity: 0,
+    },
+    visible: {
+      opacity: 1,
+      transition: {
+        duration: 1,
+      },
+    },
+  };
+
+  const contentVariants = {
+    hidden: {
+      y: '-100vw',
+      opacity: 0,
+    },
+    visible: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        type: 'spring',
+        delay: 1,
+        damping: 13,
+      },
+    },
+  };
+
+  const contactVariants = {
+    hidden: {
+      scale: 0,
+    },
+    visible: {
+      scale: 1,
+      transition: {
+        type: 'spring',
+        delay: 1,
+      },
+    },
+  };
+
   return (
-    <div className='user'>
+    <motion.div
+      variants={cointainerVariants}
+      className='user'
+      initial='hidden'
+      animate='visible'
+    >
       <div className={isDarkMode ? 'user-container dark' : 'user-container'}>
         <div className='user-container__keys'>
           <p>{content.firstName}:</p>
@@ -44,9 +91,9 @@ const User = () => {
           <p className='password'>{'*'.repeat(password?.length)}</p>
           <p># # # #</p>
         </div>
-        <div className='user-container__icon'>
+        <motion.div variants={contentVariants} className='user-container__icon'>
           <Profile />
-        </div>
+        </motion.div>
         <div className='logOutBtn'>
           <Btn
             text={content.logOut}
@@ -55,9 +102,11 @@ const User = () => {
           />
         </div>
       </div>
-      <p>{content.contact}: kurtguardia@gmail.com</p>
+      <motion.p variants={contactVariants}>
+        {content.contact}: kurtguardia@gmail.com
+      </motion.p>
       <small>{content.copyright}</small>
-    </div>
+    </motion.div>
   );
 };
 
